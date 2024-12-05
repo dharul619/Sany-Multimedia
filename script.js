689f22869ee60e9809fd5048c7aa69641e952d82
dharul <dharulyuswansyah619@gmail.com>
2024-12-05 18:42:38 +0700

    perubahan pada service

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ prev.addEventListener("click", function () {
   let items = document.querySelectorAll(".item");
   document.querySelector(".slide").prepend(items[items.length - 1]); // here the length of items = 6
 });
+
+const openBtn = document.getElementById("submit");
+const closeBtn = document.getElementById("closemodal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("open");
+});
